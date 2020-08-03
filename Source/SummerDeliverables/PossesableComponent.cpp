b02bafd98450024da3e3ba0d724f650b523bb365
Nick Ludowese <nludowese@yahoo.com>
2020-08-03 13:04:34 -0400

    updated possesable

--- Source/SummerDeliverables/PossesableComponent.cpp
+++ Source/SummerDeliverables/PossesableComponent.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
@@ -14,3 +15,37 @@ void UPossesableComponent::EndInteract_Implementation()
 {
     bInUse=false;
 }
+=======
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PossesableComponent.h"
+
+void UPossesableComponent::OnInteract_Implementation()
+{
+   APawn* Owner=  Cast<APawn>(GetOwner());
+    if (Owner==nullptr)
+    {
+        //send error message here
+        return;
+    }
+   bInUse=true;
+    CurrentPlayer=Owner->GetController();
+}
+
+void UPossesableComponent::EndInteract_Implementation()
+{
+    bInUse=false;
+    CurrentPlayer=nullptr;
+}
+
+void UPossesableComponent::TickComponent(float deltaTime)
+{
+    if(CurrentCooldown>0)
+    {
+        CurrentCooldown-=deltaTime;
+    }  
+}
+
+
+>>>>>>> Stashed changes
--- Source/SummerDeliverables/PossesableComponent.h
+++ Source/SummerDeliverables/PossesableComponent.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
@@ -52,3 +53,61 @@ class SUMMERDELIVERABLES_API UPossesableComponent : public UInteractableComponen
 		float DamageAmount;
 
 };
+=======
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractableComponent.h"
+#include "GameFramework/Pawn.h"
+#include "PossesableComponent.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class SUMMERDELIVERABLES_API UPossesableComponent : public UInteractableComponent
+{
+	GENERATED_BODY()
+	public:
+		
+		virtual void OnInteract_Implementation() override;
+		virtual void EndInteract_Implementation() override;
+		UFUNCTION(BlueprintNativeEvent, Category="InputEvent")
+		void TakeAction();
+	
+		virtual void TakeAction_Implementation(){};
+	
+		UFUNCTION(BlueprintNativeEvent, Category="InputEvent")
+		void MoveRightAxis(float Axis,float DeltaTime);
+	
+		virtual void MoveRightAxis_Implementation(float Axis,float DeltaTime){GetOwner()->AddActorLocalRotation(FQuat(0.0,0.0,DeltaTime*Axis,0.0));};//this function is a default implementation and should never be called. MoveRightAction should call it
+	
+		UFUNCTION(BlueprintNativeEvent, Category="InputEvent")
+		void MoveForwardAxis(float Axis,float DeltaTime);
+	
+		virtual void MoveForwardAxis_Implementation(float Axis,float DeltaTime){GetOwner()->AddActorLocalOffset(FVector(DeltaTime*Axis,0.0,0.0));};
+
+		virtual void TickComponent(float deltaTime);
+		//Other Virtual inputs as necessary
+	private:
+		AController* CurrentPlayer = nullptr;
+		float CurrentCooldown =0.0f;
+	public:
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components")
+		USceneComponent * ExitPoint;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+		float StamDrainRate;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+		float StamFrontCost;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+		float MaxCooldown;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+		float ParanoiaAmount;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+		float DamageAmount;
+
+};
+>>>>>>> Stashed changes
