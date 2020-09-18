8b8193d55fe6767141b7fa674a35ca43df40427a
Shawn Turk <shawnturk33@gmail.com>
2020-09-17 21:03:29 -0400

    Documented PossessablePawn.h

--- Source/SummerDeliverables/InteractSystem/PossessablePawn.h
+++ Source/SummerDeliverables/InteractSystem/PossessablePawn.h
@@ -13,42 +13,48 @@ class SUMMERDELIVERABLES_API APossessablePawn : public APawn
 {
     GENERATED_BODY()
 public:
-    // Constructor and destructor
+    /// Constructor and destructor
 	APossessablePawn();
 
 protected:
-	
+	/// Called when an instance of this class is placed (in editor) or spawned.
 	virtual void OnConstruction(const FTransform & Transform) override;
 	
 protected:
-    // Reference to currently possessing player, used for un-possessing.
+    /// Reference to currently possessing player, used for un-possessing.
     class APlayerPawn* CurrentPlayer;
     
 public:
-
+	/// Setter for CurrentPlayer pointer
 	void setPlayer(APlayerPawn * p){CurrentPlayer = p;}
-    // Called every frame
+	
+    /// Called every frame
     virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+	/// Called to bind functionality to input
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	// Relocates the current player pawn to exit point and possesses it.
+	/// Relocates the current player pawn to exit point and possesses it.
 	virtual void EndPossession();
     
 public:
+	/// A scenecomponent for providing an exit location to spawn an unpossessing player
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class USceneComponent* ExitPoint;
-    
+
+	/// The attached component that allows this pawn to be a possessable
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UPossesableComponent * PossessableComponent;
-	
+
+	/// An in-editor drop down to choose which subclass of PossessableComponent to attach
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
 	class TSubclassOf<UPossesableComponent> PossessableComponentType;
-		
+
+	/// A pointer to attach a potential skeletal mesh
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class USkeletalMeshComponent * SkeletalMeshComponent;
-		
+
+	/// A pointer to attach a potential static mesh
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UStaticMeshComponent * StaticMeshComponent;
 };
