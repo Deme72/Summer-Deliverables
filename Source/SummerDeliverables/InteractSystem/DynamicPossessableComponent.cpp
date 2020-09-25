c36875933ec14bcddd4f5b9991149161f7c9479e
Elijah Maloy <elijah.maloy10@gmail.com>
2020-09-25 14:12:18 -0400

    Tick now works properly
    FlappyBook application of it

--- Source/SummerDeliverables/InteractSystem/DynamicPossessableComponent.cpp
+++ Source/SummerDeliverables/InteractSystem/DynamicPossessableComponent.cpp
@@ -3,6 +3,12 @@
 
 #include "DynamicPossessableComponent.h"
 
+UDynamicPossessableComponent::UDynamicPossessableComponent():UPossesableComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bStartWithTickEnabled = true;
+}
+
 void UDynamicPossessableComponent::MoveRightAxis_Implementation(float Axis)
 {
     GetOwner()->AddActorLocalRotation(FQuat(0.0,0.0,Axis,0.0));
