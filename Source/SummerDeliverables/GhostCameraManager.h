9acd6efecb52f0a55713fa2731d6cf9650666bd8
Jefferey Schlueter <gods754513@gmail.com>
2020-10-21 14:31:18 -0400

    Added a GhostCameraManager class and rigged it to APlayerGhostController

---
+++ Source/SummerDeliverables/GhostCameraManager.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "GhostCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUMMERDELIVERABLES_API AGhostCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+
+	AGhostCameraManager();
+};
