1114c4391e6a521766e2c76674406204b964dbce
Anthony Carter <anthonywc117@sbcglobal.net>
2020-11-04 21:22:55 -0500

    Enemies now destroy held treasure object when destroyed
    Removed functionality from exits

--- Source/SummerDeliverables/BaseEnemyCharacter.cpp
+++ Source/SummerDeliverables/BaseEnemyCharacter.cpp
@@ -170,6 +170,14 @@ void ABaseEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInput
 	
 }
 
+// Called before this enemy would be destroyed
+void ABaseEnemyCharacter::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	//Super::EndPlay();
+	if (IsValid(treasureActor))
+		treasureActor->Destroy();
+}
+
 void ABaseEnemyCharacter::ParanoiaTick(float DeltaTime)
 {
 	if (ParanoiaDecayTime <= 0.0f)
