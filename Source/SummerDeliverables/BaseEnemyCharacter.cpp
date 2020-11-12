2f3561ac5ef4b689f65015f35358fd9f36ecd124
Anthony Carter <anthonywc117@sbcglobal.net>
2020-11-12 08:41:08 -0500

    Push missing changes
    Not sure if these are the reason for the error, worth a shot

--- Source/SummerDeliverables/BaseEnemyCharacter.cpp
+++ Source/SummerDeliverables/BaseEnemyCharacter.cpp
@@ -12,8 +12,6 @@ ABaseEnemyCharacter::ABaseEnemyCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	SkeletalMesh 
 }
 
 void ABaseEnemyCharacter::SetLastScareDirection()
