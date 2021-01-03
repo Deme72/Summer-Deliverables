54072816b365ee05df896fdcfbff4e99834452d5
Bash <sebastiankurg@gmail.com>
2021-01-03 15:17:22 -0500

    Cleanup - deleting assets

--- Source/Polterheist/Polterheist.Build.cs
+++ Source/Polterheist/Polterheist.Build.cs
@@ -13,10 +13,8 @@ public class Polterheist : ModuleRules
 			"Core", "CoreUObject", "Engine", "InputCore", "AIModule", "UMG"
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
